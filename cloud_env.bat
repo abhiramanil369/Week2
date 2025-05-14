8075d504ecd48000c95eaf9fe257a8d4f5edbc66
abhhiramanil369 <abhiramforcoding369@gmail.com>
2025-05-14 12:32:20 +0530

    google cloud initialised

--- cloud_env.bat
+++
@@ -1,7 +0,0 @@
-ECHO OFF
-CLS
-SET PATH=C:\Users\abhir\OneDrive\Documents\Summer Internship 2025\Week2\google-cloud-sdk\bin;%PATH%;
-cd C:\Users\abhir\OneDrive\Documents\Summer Internship 2025\Week2
-ECHO Welcome to the Google Cloud CLI! Run "gcloud -h" to get the list of available commands.
-ECHO ---
-ECHO ON
